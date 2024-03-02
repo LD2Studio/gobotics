e7e48aaaf3fb44092617dbaf75ce920b02afbfa2
Laurent Dethoor <ld2studiogame@gmail.com>
2024-03-02 10:54:54 +0100

    Set Gobotics v1.1.0-dev1

--- build_exports/build.py
+++ build_exports/build.py
@@ -16,7 +16,7 @@ except FileNotFoundError:
     print("project.godot doesn't found")
     
 
-py_version = "0.8.0"
+py_version = "0.10.0"
 
 executable = {
     "linux": "gobotics.x86_64",
@@ -31,36 +31,6 @@ subprocess.call(["touch", "exports/.gdignore"])
 for platform in ["linux", "windows"]:
 
     os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo/meshes")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots/meshes")
-    # os.mkdir("exports/gobotics-"+ version +"-"+ platform+"-x86_64/pygobotics")
-
-    # shutil.copy("assets/demo/ball.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/cube.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/robot.urdf", "exports/gobotics-"+ version +"-"+platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/robot_wrench.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/robot_mecanum.urdf", "exports/gobotics-"+ version +"-"+platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/robot_mecanum_wrench.urdf", "exports/gobotics-"+ version +"-"+platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/servos.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/slider.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/demo/rotations.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/demo")
-    # shutil.copy("assets/robots/robot_diff.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots")
-    # shutil.copy("assets/robots/robot_diff_raycast.urdf", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots")
-    # shutil.copy("assets/robots/meshes/PioneerCaster.glb", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots/meshes")
-    # shutil.copy("assets/robots/meshes/PioneerChassis.glb", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots/meshes")
-    # shutil.copy("assets/robots/meshes/PioneerWheel.glb", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/assets/robots/meshes")
-
-
-    # shutil.copy("pygobotics/dist/pygobotics-"+ py_version +"-py3-none-any.whl", "exports/gobotics-"+ version +"-"+ platform+"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/hello_gobotics.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/play_with_servo.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/play_with_robot.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/play_with_mecanum_robot.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/play_with_state_machine.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
-    # shutil.copy("pygobotics/play_with_rotations.py", "exports/gobotics-"+ version +"-"+ platform +"-x86_64/pygobotics/")
 
     subprocess.run(["godot4_2" , "--export-debug", "Gobotics-"+ platform , "exports/gobotics-"+ version +"-"+ platform + "-x86_64/" + executable[platform], "--headless"])
 
