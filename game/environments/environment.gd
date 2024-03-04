65b070bd7d936221c1f764031bcd4d03ac2a7941
Laurent Dethoor <ld2studiogame@gmail.com>
2024-03-04 16:03:27 +0100

    Fixed environment transparency

--- game/environments/environment.gd
+++ game/environments/environment.gd
@@ -8,6 +8,10 @@ signal asset_exited(asset: Node3D)
 func _ready() -> void:
 	living_area.add_to_group("PHYSICS_AREA")
 	living_area.body_exited.connect(_on_body_exited)
+	for visual_node in get_tree().get_nodes_in_group("VISUAL"):
+		if visual_node.owner == self:
+			var mat: BaseMaterial3D = visual_node.get_surface_override_material(0)
+			mat.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
 
 
 func _on_body_exited(body):
