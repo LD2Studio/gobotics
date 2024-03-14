17c244bbbd400b1a2a648043a0c632d0679b3b63
Laurent Dethoor <ld2studiogame@gmail.com>
2024-03-14 16:14:21 +0100

    Added physics to magnet actuator

--- game/robot_features/actuators/electromagnet/electro_magnet.gd
+++ game/robot_features/actuators/electromagnet/electro_magnet.gd
@@ -1,25 +1,45 @@
-extends Node3D
+extends Area3D
 
-
-@export var activate: bool = false:
+var activate: bool = false:
 	set(value):
 		activate = value
-		if is_instance_valid(force_area):
-			force_area.visible = true if activate else false
+		if is_instance_valid(support):
+			#print("%s activated" % [name])
+			if activate:
+				support.get_surface_override_material(0).albedo_color = Color.RED
+				gravity = 50
+				set_physics_process(true)
+			else:
+				support.get_surface_override_material(0).albedo_color = Color.WHITE
+				gravity = 0
+				set_physics_process(false)
 
-@export var magnet_radius: float = 0.01:
+var magnet_radius: float = 0.01:
 	set(new_radius):
 		magnet_radius = new_radius
 		if is_instance_valid(support):
 			var mesh: CylinderMesh = support.mesh
 			mesh.top_radius = magnet_radius * GPSettings.SCALE
 			mesh.bottom_radius = magnet_radius * GPSettings.SCALE
+			var magnet_mesh: CylinderMesh = support.get_node("MagnetArea").mesh
+			magnet_mesh.top_radius = magnet_radius * GPSettings.SCALE * 0.7
+			magnet_mesh.bottom_radius = magnet_radius * GPSettings.SCALE * 0.7
+			var magnet_col_shape: CylinderShape3D = magnet_collision.shape
+			magnet_col_shape.radius = magnet_radius * GPSettings.SCALE
+			magnet_col_shape.height = magnet_radius * GPSettings.SCALE * 2
+			magnet_collision.get_node("DebugMesh").mesh = magnet_col_shape.get_debug_mesh()
 
 @onready var support: MeshInstance3D = $Support
-@onready var force_area: MeshInstance3D = $Support/ForceArea
+@onready var magnet_collision: CollisionShape3D = $MagnetCollision
 
-
-func set_physics(enable: bool):
-	pass
-	#print("[%s] physics: %s" % [name, enable])
+func _ready():
+	activate = false
+	magnet_radius = 0.04
 	
+
+func _physics_process(_delta: float) -> void:
+	if has_overlapping_bodies():
+		#print("overlapping bodies: ", get_overlapping_bodies())
+		for body in get_overlapping_bodies():
+			if body is RigidBody3D:
+				body.sleeping = false
